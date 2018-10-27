489dcacb36ac17b37799b7747fc57c64d30c0b34
Zenon Chrysostomou <zenon1020@me.com>
2018-10-27 11:16:47 -0600

    Added endpoints to access the db through the Ionic App

---
+++ plugins/ahmadfatoni/apigenerator/controllers/api/getReceiptsController.php
@@ -0,0 +1,101 @@
+<?php namespace AhmadFatoni\ApiGenerator\Controllers\API;
+
+use Cms\Classes\Controller;
+use BackendMenu;
+
+use Illuminate\Http\Request;
+use AhmadFatoni\ApiGenerator\Helpers\Helpers;
+use Illuminate\Support\Facades\Validator;
+use AhmadFatoni\ApiGenerator\Models\ApiGenerator;
+class getReceiptsController extends Controller
+{
+	protected $ApiGenerator;
+
+    protected $helpers;
+
+    public function __construct(ApiGenerator $ApiGenerator, Helpers $helpers)
+    {
+        parent::__construct();
+        $this->ApiGenerator    = $ApiGenerator;
+        $this->helpers          = $helpers;
+    }
+
+    public function index(){
+
+        $data = $this->ApiGenerator->all()->toArray();
+
+        return $this->helpers->apiArrayResponseBuilder(200, 'success', $data);
+    }
+
+    public function show($id){
+
+        $data = $this->ApiGenerator->where('id',$id)->first();
+
+        if( count($data) > 0){
+
+            return $this->helpers->apiArrayResponseBuilder(200, 'success', $data);
+
+        }
+
+        $this->helpers->apiArrayResponseBuilder(400, 'bad request', ['error' => 'invalid key']);
+
+    }
+
+    public function store(Request $request){
+
+    	$arr = $request->all();
+
+        while ( $data = current($arr)) {
+            $this->ApiGenerator->{key($arr)} = $data;
+            next($arr);
+        }
+
+        $validation = Validator::make($request->all(), $this->ApiGenerator->rules);
+        
+        if( $validation->passes() ){
+            $this->ApiGenerator->save();
+            return $this->helpers->apiArrayResponseBuilder(201, 'created', ['id' => $this->ApiGenerator->id]);
+        }else{
+            return $this->helpers->apiArrayResponseBuilder(400, 'fail', $validation->errors() );
+        }
+
+    }
+
+    public function update($id, Request $request){
+
+        $status = $this->ApiGenerator->where('id',$id)->update($data);
+    
+        if( $status ){
+            
+            return $this->helpers->apiArrayResponseBuilder(200, 'success', 'Data has been updated successfully.');
+
+        }else{
+
+            return $this->helpers->apiArrayResponseBuilder(400, 'bad request', 'Error, data failed to update.');
+
+        }
+    }
+
+    public function delete($id){
+
+        $this->ApiGenerator->where('id',$id)->delete();
+
+        return $this->helpers->apiArrayResponseBuilder(200, 'success', 'Data has been deleted successfully.');
+    }
+
+    public function destroy($id){
+
+        $this->ApiGenerator->where('id',$id)->delete();
+
+        return $this->helpers->apiArrayResponseBuilder(200, 'success', 'Data has been deleted successfully.');
+    }
+
+
+    public static function getAfterFilters() {return [];}
+    public static function getBeforeFilters() {return [];}
+    public static function getMiddleware() {return [];}
+    public function callAction($method, $parameters=false) {
+        return call_user_func_array(array($this, $method), $parameters);
+    }
+    
+}
--- plugins/ahmadfatoni/apigenerator/routes.php
+++ plugins/ahmadfatoni/apigenerator/routes.php
@@ -4,5 +4,10 @@ Route::post('fatoni/generate/api', array('as' => 'fatoni.generate.api', 'uses' =
 Route::post('fatoni/update/api/{id}', array('as' => 'fatoni.update.api', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\ApiGeneratorController@updateApi'));
 Route::get('fatoni/delete/api/{id}', array('as' => 'fatoni.delete.api', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\ApiGeneratorController@deleteApi'));
 
-Route::resource('halo', 'AhmadFatoni\ApiGenerator\Controllers\API\haloController', ['except' => ['destroy', 'create', 'edit']]);
-Route::get('halo/{id}/delete', ['as' => 'halo.delete', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\API\haloController@destroy']);
+
+Route::resource('api/v1/getReceipt', 'AhmadFatoni\ApiGenerator\Controllers\API\getReceiptsController', ['except' => ['destroy', 'create', 'edit']]);
+Route::get('api/v1/getReceipt/{id}/delete', ['as' => 'api/v1/getReceipt.delete', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\API\getReceiptsController@destroy']);
+
+Route::resource('api/v1/getItems', 'AhmadFatoni\ApiGenerator\Controllers\API\getItemsController', ['except' => ['destroy', 'create', 'edit']]);
+Route::get('api/v1/getItems/{id}/delete', ['as' => 'api/v1/getItems.delete', 'uses' => 'AhmadFatoni\ApiGenerator\Controllers\API\getItemsController@destroy']);
+
