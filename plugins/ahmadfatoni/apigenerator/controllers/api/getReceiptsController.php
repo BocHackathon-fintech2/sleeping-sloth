4757d5f8cfd18de0d2462821ffff4c5e3e593de5
Zenon Chrysostomou <zenon1020@me.com>
2018-10-27 17:26:48 -0600

    Added cros origin domain headers

--- plugins/ahmadfatoni/apigenerator/controllers/api/getReceiptsController.php
+++ plugins/ahmadfatoni/apigenerator/controllers/api/getReceiptsController.php
@@ -1,5 +1,8 @@
 <?php namespace AhmadFatoni\ApiGenerator\Controllers\API;
-header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization');
 
 use DB;
 
