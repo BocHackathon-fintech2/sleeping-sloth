9d2de4d1f1709dae6f0cdff585d091e95b7e0dea
Zenon Chrysostomou <zenon1020@me.com>
2018-10-27 06:47:12 -0600

    Created the layout for the POS. Created a plugin to input data in the POS

---
+++ plugins/sleepingsloth/posdata/updates/builder_table_create_sleepingsloth_posdata_.php
@@ -0,0 +1,27 @@
+<?php namespace Sleepingsloth\Posdata\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateSleepingslothPosdata extends Migration
+{
+    public function up()
+    {
+        Schema::create('sleepingsloth_posdata_', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('price');
+            $table->string('vat');
+            $table->string('item_category');
+            $table->string('retailer_category');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('sleepingsloth_posdata_');
+    }
+}
