5e50d2eae8105fa614ef08d7827bacaefca10ec5
Zenon Chrysostomou <zenon1020@me.com>
2018-10-27 10:57:25 -0600

    Added the ability to push the items on the DB. Added controlls on the back end to view the items DB

--- themes/hambern-hambern-blank-bootstrap-4/assets/pos.js
+++ themes/hambern-hambern-blank-bootstrap-4/assets/pos.js
@@ -4,7 +4,9 @@ $(document).ready( function(){
        var price = parseFloat($(this).data('price')) ; 
        var name = $(this).data('name') ; 
        
-        $('#resultTable tr:last').after('<tr><td>'+'1'+'</td><td>'+name+'</td><td>'+price+'</td></tr>');
+       var category = $(this).data('category') ; 
+       
+        $('#resultTable tr:last').after('<tr data-price="'+price+'" data-name="'+name+'" data-category="'+category+'"><td>'+'1'+'</td><td>'+name+'</td><td>'+price+'</td></tr>');
         
        var total = parseFloat($("#totalPrice").val());
        console.log(total);
@@ -44,7 +46,39 @@ $(document).ready( function(){
                    data: { total: total, phone: phone, category: category, vendorName: vendorName, location:location  },
                    success: function(data) {
                          this.success(data).done(function() {
-                            console.log(data);
+                            var id = data["result"];
+                            console.log('ID='+id);
+                            var counter = 0;
+                             $('#resultTable tr').each(function (i, row) {
+                               
+                                 
+                                if(counter !=0){
+                                    
+                                      console.log($(this).data('price'));
+                                 console.log($(this).data('name'));
+                                 console.log($(this).data('category'));
+                                 
+                                 var itemPrice = $(this).data('price');
+                                 var itemName = $(this).data('name');
+                                 var itemCategory = $(this).data('category');
+                                 
+                                    $.request('onSendItem', {
+                                       //pass the inputed data code & descritpion to the ajax call
+                                       data: { id: id, price: itemPrice, name: itemName, category:itemCategory, quantity: "1"  },
+                                       success: function(data) {
+                                             this.success(data).done(function() {
+                                               console.log('succsess');
+                                                
+                                                
+                                                
+                                                
+                                             })
+                                       }});
+                                      
+                                }
+                               counter = 1; 
+                             });//end for tr
+                            
                          })
                    }});
                    
