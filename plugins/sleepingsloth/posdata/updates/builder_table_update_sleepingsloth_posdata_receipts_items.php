9b3f7d69376081485ac141c89b9e950408a4b43b
Zenon Chrysostomou <zenon1020@me.com>
2018-10-27 21:04:44 -0600

    Changed the DB schema for the items table so that the price is a string instead of an int

---
+++ plugins/sleepingsloth/posdata/updates/builder_table_update_sleepingsloth_posdata_receipts_items.php
@@ -0,0 +1,23 @@
+<?php namespace Sleepingsloth\Posdata\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateSleepingslothPosdataReceiptsItems extends Migration
+{
+    public function up()
+    {
+        Schema::table('sleepingsloth_posdata_receipts_items', function($table)
+        {
+            $table->string('price', 10)->nullable(false)->unsigned(false)->default(null)->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('sleepingsloth_posdata_receipts_items', function($table)
+        {
+            $table->integer('price')->nullable(false)->unsigned(false)->default(null)->change();
+        });
+    }
+}
